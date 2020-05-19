ee70a08efd85316285f01fd4658e65844ace119e
Clarissa1603 <cynthia.clarissa@hotmail.com>
2020-05-18 19:18:55 -0500

    refactore: renaming the proyect's file jerarchy

--- Generador_Titulos/src/main/java/com/clarissa/generador_titulos/JFT_Generador_Titulos.java
+++ Generador_Titulos/src/main/java/com/clarissa/generador_titulos/JFT_Generador_Titulos.java
@@ -1,3 +1,5 @@
+package com.clarissa.generador_titulos;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
